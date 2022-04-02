58a76bd99ba6d863caa1f6ad6f3b3f70985151f0
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-04-02 11:01:37 +0200

    feat: first release

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # realworld-api-action
 GitHub Action running a Postman collection to test the compliance of a backend implementation of RealWorld specs
+
+## Inputs
+
+|          | required | description |
+|----------|----------|-------------|
+| url      | true     | url of the api you are willing to test            |
+| username | false    | username used for the tests (must be unique)            |
+| email    | false    | email used for the tests (must be unique)            |
+| password | false    | password used for the tests            |
---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const newman = require('newman');
+
+const username = core.getInput('USERNAME') || `${github.context.issue.owner}-${new Date().toString()}`;
+const email = core.getInput('EMAIL') `${username}@mail.com`;
+const password = core.getInput('PASSWORD') || (Math.random() * 12).toString(36)
+
+newman.run({
+    delayRequest: 500,
+    globals: {
+        url: core.getInput('APIURL'),
+        username,
+        email,
+        password
+    },
+    collection: require('./postman-collection.json'),
+    reporters: 'cli'
+},  (err) =>  {
+    if (err) { core.setFailed(err) }
+});
