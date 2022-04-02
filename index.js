2cd1373f3e01cb27ab09799e265b142f92a43d8f
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-04-02 11:15:04 +0200

    fix: inputs naming

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const newman = require('newman');
 
-const username = core.getInput('USERNAME') || `${github.context.issue.owner}-${new Date().toString()}`;
-const email = core.getInput('EMAIL') || `${username}@mail.com`;
-const password = core.getInput('PASSWORD') || (Math.random() * 12).toString(36)
+const username = core.getInput('username') || `${github.context.issue.owner}-${new Date().toString()}`;
+const email = core.getInput('email') || `${username}@mail.com`;
+const password = core.getInput('password') || (Math.random() * 12).toString(36)
 
 newman.run({
     delayRequest: 500,
     globals: {
-        url: core.getInput('APIURL'),
+        url: core.getInput('url'),
         username,
         email,
         password
