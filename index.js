b1f77add5fa519143cffeedb69098c9d304b68bf
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-04-02 11:13:18 +0200

    fix: foo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const github = require('@actions/github');
 const newman = require('newman');
 
 const username = core.getInput('USERNAME') || `${github.context.issue.owner}-${new Date().toString()}`;
-const email = core.getInput('EMAIL') `${username}@mail.com`;
+const email = core.getInput('EMAIL') || `${username}@mail.com`;
 const password = core.getInput('PASSWORD') || (Math.random() * 12).toString(36)
 
 newman.run({
