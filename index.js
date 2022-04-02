93f4dac7731cdf5ccf9ff3d828343c2296454cb1
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-04-02 11:23:29 +0200

    fix: globals

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ const password = core.getInput('password') || (Math.random() * 12).toString(36)
 
 newman.run({
     delayRequest: 500,
-    globals: {
-        APIURL: core.getInput('url'),
-        USERNAME: username,
-        EMAIL: email,
-        PASSWORD: password
-    },
+    globals: [
+        {APIURL: core.getInput('url')},
+        {USERNAME: username},
+        {EMAIL: email},
+        {PASSWORD: password}
+    ],
     collection: require('./postman-collection.json'),
     reporters: 'cli'
-},  (err) =>  {
-    if (err) { core.setFailed(err) }
+}, (err) => {
+    if (err) {
+        core.setFailed(err)
+    }
 });
