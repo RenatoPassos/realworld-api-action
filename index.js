29cf59aab7bf96802c04f8dcff03af23666679d0
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-04-08 20:03:07 +0200

    add command

--- index.js
+++ index.js
@@ -9,26 +9,32 @@ const password = core.getInput('password') || (Math.random() * 12).toString(36);
 
 const startCommand = core.getInput('start');
 
-if (startCommand) {
-    await exec.exec(startCommand);
+const main = async () => {
+    if (startCommand) {
+        await exec.exec(startCommand);
+    }
+
+    newman.run({
+        delayRequest: 500,
+        globalVar: [
+            {key: 'APIURL', value: core.getInput('url')},
+            {key: 'USERNAME', value: username},
+            {key: 'EMAIL', value: email},
+            {key: 'PASSWORD', value: password},
+        ],
+        collection: require('./postman-collection.json'),
+        reporters: 'cli'
+    }).on('done', (err, summary) => {
+        if (err || summary.run.error) {
+            core.setFailed('collection run encountered an error.')
+        } else if (summary.run.failures.length) {
+            core.setFailed( `${summary.run.failures.length} failure${summary.run.failures.length > 1 ? 's' : ''}.`);
+        } else {
+            console.log('collection run completed.');
+        }
+    });
 }
 
-newman.run({
-    delayRequest: 500,
-    globalVar: [
-        {key: 'APIURL', value: core.getInput('url')},
-        {key: 'USERNAME', value: username},
-        {key: 'EMAIL', value: email},
-        {key: 'PASSWORD', value: password},
-    ],
-    collection: require('./postman-collection.json'),
-    reporters: 'cli'
-}).on('done', (err, summary) => {
-    if (err || summary.run.error) {
-        core.setFailed('collection run encountered an error.')
-    } else if (summary.run.failures.length) {
-        core.setFailed( `${summary.run.failures.length} failure${summary.run.failures.length > 1 ? 's' : ''}.`);
-    } else {
-        console.log('collection run completed.');
-    }
-});
+main();
+
+
