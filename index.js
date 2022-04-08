39bf58200d799bba5d9c96e28793b2a0c77c40ea
geromegrignon <gerome.grignon.lp2@gmail.com>
2022-04-08 20:18:33 +0200

    add command

--- index.js
+++ index.js
@@ -9,30 +9,29 @@ const password = core.getInput('password') || (Math.random() * 12).toString(36);
 
 const startCommand = core.getInput('start');
 
-const main = async () => {
-    if (startCommand) {
-        await exec.exec(startCommand);
-    }
-
-    newman.run({
-        delayRequest: 500,
-        globalVar: [
-            {key: 'APIURL', value: core.getInput('url')},
-            {key: 'USERNAME', value: username},
-            {key: 'EMAIL', value: email},
-            {key: 'PASSWORD', value: password},
-        ],
-        collection: require('./postman-collection.json'),
-        reporters: 'cli'
-    }).on('done', (err, summary) => {
-        if (err || summary.run.error) {
-            core.setFailed('collection run encountered an error.')
-        } else if (summary.run.failures.length) {
-            core.setFailed( `${summary.run.failures.length} failure${summary.run.failures.length > 1 ? 's' : ''}.`);
-        } else {
-            console.log('collection run completed.');
-        }
-    });
+const main = () => {
+    exec.exec(startCommand).then(() => {
+        newman.run({
+            delayRequest: 500,
+            globalVar: [
+                {key: 'APIURL', value: core.getInput('url')},
+                {key: 'USERNAME', value: username},
+                {key: 'EMAIL', value: email},
+                {key: 'PASSWORD', value: password},
+            ],
+            collection: require('./postman-collection.json'),
+            reporters: 'cli'
+        }).on('done', (err, summary) => {
+            if (err || summary.run.error) {
+                core.setFailed('collection run encountered an error.')
+            } else if (summary.run.failures.length) {
+                core.setFailed( `${summary.run.failures.length} failure${summary.run.failures.length > 1 ? 's' : ''}.`);
+            } else {
+                console.log('collection run completed.');
+            }
+        });
+        process.exit(0);
+    })
 }
 
 main();
